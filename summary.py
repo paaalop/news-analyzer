26b6911844241b8f2e7cf6b2e6244c205108fb5e
paaalop <palop020912@gmail.com>
2025-06-13 01:34:57 +0900

    6/13 요약 페이지, 페이지네이션

--- summary.py
+++ summary.py
@@ -13,7 +13,7 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",
     handlers=[
-        logging.FileHandler("news_summary.log"),
+        logging.FileHandler("./logs/summary.log"),
         logging.StreamHandler()
     ]
 )
